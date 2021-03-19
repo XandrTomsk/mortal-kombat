5e39eaec694643debf7e6470d01fe3828eb69dcc
Xandr <63450107+XandrTomsk@users.noreply.github.com>
2021-03-19 13:01:55 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#app').vide({
+      mp4: 'video/video.mp4',
+      webm: 'video/video.ogv',
+      ogv: 'video/video.webm',
+      poster: 'img/cover-image.jpg' 
+    });
+
+});
